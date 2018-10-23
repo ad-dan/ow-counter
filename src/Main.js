5014f43f11f143cf403e1e4139b2192638f62052
Ad <adunreal@gmail.com>
2018-10-23 17:02:06 +0530

    Added main

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Score: 0</Text>
+      </View>
+    );
+  }
+}
